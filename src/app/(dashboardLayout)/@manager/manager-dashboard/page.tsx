e062e152f2599186cc50757d5e54b98cfc1df67e
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2026-04-15 11:39:19 +0600

    manager profile done

--- src/app/(dashboardLayout)/@manager/manager-dashboard/page.tsx
+++ src/app/(dashboardLayout)/@manager/manager-dashboard/page.tsx
@@ -1,9 +1,13 @@
+import Profile from '@/components/module/shared/Profile';
+import { AuthServices } from '@/services/auth.services';
 import React from 'react';
 
-const ManagerPage = () => {
+const ManagerPage = async() => {
+ const {user}= await AuthServices.session()
+ 
     return (
         <div>
-            im manager
+            <Profile user={user}></Profile>
         </div>
     );
 };
